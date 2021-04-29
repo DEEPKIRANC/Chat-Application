3d69dacc44b1ba7cb756bc489a59c7820c928ad2
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-30 03:28:04 +0530

    Style - Change styling of sidebar component

--- chat-application/src/components/sidebar.js
+++ chat-application/src/components/sidebar.js
@@ -6,8 +6,8 @@ function Sidebar() {
     return (
         <div className="sidebar__component">
             <div className="header">
-                <Avatar style={{width:'3rem',height:'3rem'}} src="../temp_images/Profile_Deep-min.jpg"/>
-                <span>Currently Active</span>
+                <Avatar style={{width:'3rem',height:'3rem'}} srcSet="../temp_images/Profile_Deep-min.jpg"/>
+                <span><div className="statusColor"></div>Currently Active</span>
                 <ExitToAppIcon />
             </div>
         </div>
