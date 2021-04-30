209b064f36ba8fd49b3cc80a2af24af8cb07b115
Deep Kiran <cdeepkiran14@gmail.com>
2021-05-01 02:29:02 +0530

    Add Firebase Config for connection

---
+++ chat-application/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseApp=firebase.initializeApp({
+    apiKey: "AIzaSyAWX1wOZJcpAKS1K2pbypyEnLfTW3XM0lI",
+  authDomain: "chat-app-a710c.firebaseapp.com",
+  projectId: "chat-app-a710c",
+  storageBucket: "chat-app-a710c.appspot.com",
+  messagingSenderId: "200832564188",
+  appId: "1:200832564188:web:fc471034cc3bab36b2ad19"
+})
+
+const db=firebaseApp.firestore();
+
+export {db,firebaseApp};
