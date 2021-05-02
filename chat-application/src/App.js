6ada68f4f908c90766cb73577f81d0a334693b3f
Deep Kiran <cdeepkiran14@gmail.com>
2021-05-02 14:28:08 +0530

    Implement useHistory hook

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 import Chatbox from './components/chatbox';
 import Sidebar from './components/sidebar';
+import {BrowserRouter as Router , Switch , Route} from "react-router-dom";
+import Test from './components/Test';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <div className="app__body">
-        <Sidebar />
-        <Chatbox />
+        <Switch>
+        
+        <Route path="/mobilechat" exact>
+
+          <Test />
+
+        </Route> 
+
+        <Route path="/" exact>  
+          <Sidebar />
+          <Chatbox />
+        </Route>
+         
+        </Switch>
       </div>  
     </div>
+    </Router>
   );
 }
 
--- chat-application/src/components/Chat.js
+++ chat-application/src/components/Chat.js
@@ -1,17 +1,28 @@
 import React,{useEffect,useState} from 'react'
 import {Avatar} from "@material-ui/core"
 import "../styles/chats.css"
+import {useMediaQuery} from "react-responsive";
+import {useHistory} from "react-router-dom";
 
 
 function Chat() {
-    
-    const [randomText,setRandomText]=useState("");    
+    let windowSize=useMediaQuery({query:`(max-width:600px)`});
+    const [randomText,setRandomText]=useState("");
+    let history=useHistory();
+        
 useEffect(()=>{
     
     setRandomText(Math.floor(Math.random() * 5000));
 },[])
 
+
 const handleClick=()=>{
+    
+    if(windowSize)
+    {
+        history.push("/mobilechat");
+    }
+   
     
 }
     
